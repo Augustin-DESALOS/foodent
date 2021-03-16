7ee3d2f182f38f5e66dd7da80fc75b3fc466d35d
augustin desalos <augustin.desalos@gmail.com>
2021-03-16 17:29:30 +0100

    seed-init

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,11 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+
+  def display_ingredients
+    recipe_ingredients.map do |ri|
+      "#{ri.quantity} #{ri.ingredient.name}"
+    end
+  end
 end
