da7a0f87cbc418c967e7f98c6af558090359c6bc
eric nguyen <grider.uleched13@gmail.com>
2021-03-16 13:51:52 +0100

    routes1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "shopping_list", to: "ingredients#shopping_list", as: :shopping_list
+  resources :lists, only: [:create,:update]
+  resources :recipes, only: [:index, :show]
 end
