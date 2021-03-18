c428dc4695a3aac2b1aca086689b81ac02528a71
eric nguyen <grider.uleched13@gmail.com>
2021-03-18 16:21:10 +0100

    search

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,13 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def search_recipes
+    if params[:query].present?
+      @recipes = Recipe.search_recipes(params[:query])
+    else
+      render :index
+    end
+end
+
 end
