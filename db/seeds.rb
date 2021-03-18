525ca51c7bfe12ad988efecdd1d3b50c7228402a
augustin desalos <augustin.desalos@gmail.com>
2021-03-18 16:58:24 +0100

    Final seed + breakfast seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'json'
-require 'open-uri'
-
-RecipeIngredient.destroy_all
-Ingredient.destroy_all
-Recipe.destroy_all
-
 # 5.times do
 #   url = "https://www.themealdb.com/api/json/v1/1/random.php"
 #   meal = JSON.parse(open(url).read)["meals"].first
@@ -48,48 +41,57 @@ Recipe.destroy_all
 #   end
 # end
 
-require "open-uri"
+
+require 'json'
+require 'open-uri'
 require "nokogiri"
+
+RecipeIngredient.destroy_all
+Ingredient.destroy_all
+Recipe.destroy_all
+
 
 html = open("https://www.marmiton.org/recettes/index/categorie/plat-principal?rcp=0").read
 # 1. Parse HTML
 doc = Nokogiri::HTML(html, nil, "utf-8")
 # 2. For the first five results
 
-doc.search(".recipe-card").first(30).each do |element|
+doc.search(".recipe-card").first(0).each do |element|
   # 3. Create recipe and store it in results
   name = element.search(".recipe-card__title").first.text.strip
   recipe_url = element.search(".recipe-card-link").first.attributes["href"].value
   description = element.search(".recipe-card__description").first.text.strip
   rating = element.search(".recipe-card__rating__value").first.text.strip
   prep_time = element.search(".recipe-card__duration__value").first.text.strip
-  recipe = Recipe.create(name: name, description: description, rating: rating, cooking_time: prep_time)
-
-   p recipe_url
-
+  
   recipe_html = open(URI.escape(recipe_url)).read
   recipe_doc = Nokogiri::HTML(recipe_html, nil, "utf-8")
+  
+  # recherche de la photo pour chaque recettes ici :
+  p picture = recipe_doc.search("#recipe-picture-print").first.attributes["src"].value
+  recipe = Recipe.create(name: name, description: description, picture: picture, rating: rating, cooking_time: prep_time)
+  recipe_url
+
   recipe_doc.search(".ingredient-list__ingredient-group li").each do |ingredient|
     ingredient_name =  ingredient.search(".item__ingredient .ingredient-name").first.text.strip
     ingredient_quantity = ingredient.search(".item__quantity .quantity").first.text.strip
     ingredient_unit = ingredient.search(".item__quantity .unit").first.text.strip
-    if !ingredient_name.blank? 
+    if !ingredient_name.blank?
       # Je regarde si je connais cet ingrédient en DB
       db_ingredient = Ingredient.find_by_name(ingredient_name)
       # Si je le connais pas
       # p ingredient_name
       html = open(URI.escape("https://www.carrefour.fr/s?q=#{ingredient_name.parameterize}")).read
       doc_price = Nokogiri::HTML(html, nil, "utf-8")
-      p "before scrap"
+      
       scrap_ingredient = doc_price.search(".ds-product-card--vertical.ds-product-card:not(.ds-product-card--shimmer)").first
-      p "after scrap"
-      p "before price"
+      
+      
       if scrap_ingredient.nil?
         ingredient_price = 100
       else
         ingredient_price = (scrap_ingredient.search(".ds-title.product-card-price__price--final").first.text.strip.gsub(",", ".").to_f)*100.to_i
       end
-      p "after scrap"
         # document.querySelector(".ds-product-card--vertical.ds-product-card:not(.ds-product-card--shimmer)").querySelector(".ds-title.product-card-price__price--final")
 
 
@@ -111,3 +113,61 @@ doc.search(".recipe-card").first(30).each do |element|
     end
   end
 end
+
+
+# 1 breakfast (bol de cereales + lait)
+
+boldecereales = Recipe.create(name: "3 céréales URSS", description: "Céréales du partie comuniste de Vladimir poutine, vous rendra riche et beau... à consomer avec du lait frais (et de la vodka pour les vrai bonhomme)", picture: "boldecereales.jpg", rating: 2, cooking_time: "5 min")
+cerealescrunchy = Ingredient.create(name: "Céréales 3 chocolats", price: 2)
+lait = Ingredient.create(name: "lait demi-écrémé", price: 1)
+recipebreakfast = RecipeIngredient.create(recipe: boldecereales, ingredient: cerealescrunchy, quantity: "100", unit: "gr")
+recipebreakfast1 = RecipeIngredient.create(recipe: boldecereales, ingredient: lait, quantity: "50", unit: "cl")
+
+petitprince = Recipe.create(name: "French biscuit", description: "Biscuit petit prince trempé 3 fois dans un bol de lait... Et n'éclabousse pas quand il tombe dans le bol parce qu'il est trop moue...", picture: "petitprince.jpg", rating: 4, cooking_time: "7 min")
+biscuitpetitprince = Ingredient.create(name: "Petit prince", price: 3)
+lait = Ingredient.create(name: "lait demi-écrémé", price: 1)
+recipepetitprince = RecipeIngredient.create(recipe: petitprince, ingredient: biscuitpetitprince, quantity: "150", unit: "gr")
+recipepetitprince1 = RecipeIngredient.create(recipe: petitprince, ingredient: lait, quantity: "50", unit: "cl")
+
+zumbacafew = Recipe.create(name: "Café noir, (le vrai)", description: "Vous avez du mal à vous réveillez le matin? Pas de problème, avec le café noir de la marque Zumba cafew, vous allez pouvoir optimiser votre journée de travail tout en dansant la zumba coréene. (à consomer avec modératation !!!)", picture: "cafew.jpg", rating: 5, cooking_time: "30 sec")
+cafew = Ingredient.create(name: "Café en grain de Corée", price: 2)
+eau = Ingredient.create(name: "L'eau", price: 0)
+recipecafew = RecipeIngredient.create(recipe: zumbacafew, ingredient: cafew, quantity: "300", unit: "gr")
+recipecafew1 = RecipeIngredient.create(recipe: zumbacafew, ingredient: eau, quantity: "1", unit: "l")
+
+tartinedesavoie = Recipe.create(name: "La tramontane", description: "Sur une tartine de pain rassis, etalez du coulomier de premier choix avec de la confiture de fraise et des lardons", picture: "tartinedesavoie.jpg", rating: 1, cooking_time: "10 min")
+pain = Ingredient.create(name: "Pain rassis", price: 0)
+coulomier = Ingredient.create(name: "Coulomier", price: 3)
+confituredefraise = Ingredient.create(name: "Confiture de fraise", price: 2)
+lardons = Ingredient.create(name: "Lardons", price: 1)
+cafew = Ingredient.create(name: "Café en grain de Corée", price: 2)
+eau = Ingredient.create(name: "L'eau", price: 0)
+recipetartinedesavoie = RecipeIngredient.create(recipe: tartinedesavoie, ingredient: cafew, quantity: "300", unit: "gr")
+recipetartinedesavoie1 = RecipeIngredient.create(recipe: tartinedesavoie, ingredient: eau, quantity: "1", unit: "l")
+recipetartinedesavoie2 = RecipeIngredient.create(recipe: tartinedesavoie, ingredient: coulomier, quantity: "100", unit: "gr")
+recipetartinedesavoie3 = RecipeIngredient.create(recipe: tartinedesavoie, ingredient: confituredefraise, quantity: "20", unit: "gr")
+recipetartinedesavoie4 = RecipeIngredient.create(recipe: tartinedesavoie, ingredient: pain, quantity: "100", unit: "gr")
+
+kingbreakfast = Recipe.create(name: "Le repas des rois", description: "Inspirez profondement et expirez doucement, répétez cette technique 3 fois et ... c'est bon vous n'avez plus faim ! Bonne journée !!!", picture: "kingbreakfast.jpg", rating: 0, cooking_time: "0 sec")
+seum = Ingredient.create(name: "Du seum", price: 0)
+recipekingbreakfast = RecipeIngredient.create(recipe: kingbreakfast, ingredient: seum, quantity: "3", unit: "co2")
+
+pancakenutella = Recipe.create(name: "Pancake nutella", description: "Pancake délicieux avec une couche généreuse de nutella, rien de mieux pour prendre 20 kg.. (pour votre santé, n'oubliez pas de mangez 5 fruits et légumes par jours)", picture: "pancakenutella.jpg", rating: 5, cooking_time: "15 min")
+farine = Ingredient.create(name: "Farine de blé", price: 1)
+nutella = Ingredient.create(name: "Nutella (pas celui qui favorise la déforestation)", price: 5)
+oeuf = Ingredient.create(name: "Oeuf de cocque", price: 1)
+sucre = Ingredient.create(name: "Sucre en poudre", price: 1)
+lait = Ingredient.create(name: "lait demi-écrémé", price: 1)
+recipepancakenutella = RecipeIngredient.create(recipe: pancakenutella, ingredient: farine, quantity: "20", unit: "gr")
+recipepancakenutella1 = RecipeIngredient.create(recipe: pancakenutella, ingredient: sucre, quantity: "10", unit: "gr")
+recipepancakenutellat2 = RecipeIngredient.create(recipe: pancakenutella, ingredient: lait, quantity: "50", unit: "cl")
+recipepancakenutellat3 = RecipeIngredient.create(recipe: pancakenutella, ingredient: oeuf, quantity: "2", unit: "unité")
+recipepancakenutellat4 = RecipeIngredient.create(recipe: pancakenutella, ingredient: nutella, quantity: "100", unit: "gr")
+
+veganbreakfast = Recipe.create(name: "Le vegan breakfast", description: "Cultivé de père en fils sur l'une des terre les plus banal de poitou-charentes, dégustez une herbe de qualité ferti-ligiène.. Bon apétit !!!", picture: "veganbreakfast.jpg", rating: 4, cooking_time: "15 min")
+herbebanal = Ingredient.create(name: "Herbe d'origin protégé", price: 1)
+terre = Ingredient.create(name: "Terre de qualité ferti-ligiène", price: 3)
+recipevegan = RecipeIngredient.create(recipe: veganbreakfast, ingredient: herbebanal, quantity: "200", unit: "gr")
+recipevegan1 = RecipeIngredient.create(recipe: veganbreakfast, ingredient: terre, quantity: "300", unit: "gr")
+
+
