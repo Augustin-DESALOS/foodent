d0d7f713696a0f0309840b97fefe21269e655ef0
eric nguyen <grider.uleched13@gmail.com>
2021-03-22 09:36:53 +0100

    e

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,9 +1,9 @@
 class IngredientsController < ApplicationController
   def shopping_list
-    @recipes = current_user.lists.last.recipes
-    @ingredients = []
-    @recipes.each do |recipe|
-      @ingredients << recipe.recipe_ingredients
-    end
+    # @recipes = current_user.lists.last.recipes
+    # @ingredients = []
+    # @recipes.each do |recipe|
+    #   @ingredients << recipe.recipe_ingredients
+    # end
   end
 end
