0fc860c18f6afd09ed68375412921e7c177b513a
eric nguyen <grider.uleched13@gmail.com>
2021-03-23 11:56:40 +0100

    form

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get "my_list", to: "pages#my_list", as: :my_list
   post "save_list", to: "pages#save_list", as: :save_list
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :ingredients, only: [:update]
   get "shopping_list", to: "ingredients#shopping_list", as: :shopping_list
+    resources :ingredients, only: [:update]
   resources :lists, only: [:create,:update]
   resources :recipes, only: [:index, :show]
   get 'recipes_search', to: 'recipes#search_recipes', as: :search_recipes
